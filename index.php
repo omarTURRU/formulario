23b0bb64192c4778ad9e3c776fa1d4e765f0ea98
Omar Turrubiartes Acosta <71800986+omarTURRU@users.noreply.github.com>
2021-10-07 22:18:11 -0500

    Add files via upload
    parte 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Proyecto parte1</title>
+	
+
+</head>
+<body>
+
+	<h1> HOLAAA </h1>
+
+		</form>
+	</div>
+</body>
+</html>
